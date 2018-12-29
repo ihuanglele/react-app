7d01d19d1dfc9a6e9f95f00935f076da365a22bf
ihuanglele <huanglele@yousuowei.cn>
2018-12-29 17:23:14 +0800

    init

---
+++ config/config.js
@@ -0,0 +1,13 @@
+export default {
+    routes: [{
+        path: '/',
+        component: './Hello',
+    }],
+
+    plugins: [
+        ['umi-plugin-react', {
+            antd: true
+        }],
+    ],
+
+};
