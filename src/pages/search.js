eb9d0a611935094fa4ca0979f2fed953776af658
ihuanglele <huanglele@yousuowei.cn>
2019-01-09 18:17:06 +0800

    for keep

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,33 +1,49 @@
-import {SearchBar, WingBlank} from 'antd-mobile';
+import {Icon, SearchBar, WingBlank} from 'antd-mobile';
+// require('./search.css');
+import styles from './search.css'
 
-class Hello extends React.Component {
+export default class Hello extends React.Component {
   state = {
     value: '搜索',
+    isSearch: false,
   };
   onChange = (value) => {
     this.setState({value})
-  }
+  };
+  search = (v) => {
+    console.log(v);
+    // this.setState({isSearch:true});
+  };
 
   render() {
     return (
       <div>
         <WingBlank>
-          <div>搜索</div>
+          <div className={styles.logo}>Search for You</div>
         </WingBlank>
         <SearchBar
           value={this.state.value}
           placeholder="搜索"
-          className="sub-title"
-          // onSubmit={value => console.log(value, 'onSubmit')}
+          onSubmit={this.search('submit')}
           // onClear={value => console.log(value, 'onClear')}
           // onFocus={() => console.log('onFocus')}
-          // onBlur={() => console.log('onBlur')}
+          onBlur={this.search('blur')}
           // onCancel={() => console.log('onCancel')}
           showCancelButton={false}
           onChange={this.onChange}/>
+
+        <WingBlank/>
+
+        {
+          this.state.isSearch ? (
+            <div className={styles.searchLoading}>
+              <Icon type="loading"/>
+            </div>
+          ) : ''
+        }
+
+
       </div>
     );
   }
 }
-
-export default Hello;
