6ab0914dcb98988393b031845f78626b3b2febcd
ihuanglele <huanglele@yousuowei.cn>
2019-01-14 23:14:07 +0800

    for keep

--- src/pages/search.js
+++ src/pages/search.js
@@ -6,34 +6,43 @@ export default class Hello extends React.Component {
   state = {
     value: '搜索',
     isSearch: false,
+    time: 1,
   };
   onChange = (value) => {
-    // this.setState({value})
+    // console.log(value);
+    this.setState({value})
   };
   search = (v) => {
     console.log(v);
-    // this.setState({isSearch:true});
+    let time = this.state.time;
+    time++;
+    setTimeout(() => {
+      this.setState({isSearch: false, time})
+    }, 3000);
   };
 
   render() {
     return (
       <div>
-        <WingBlank>
-          <div className={styles.logo}>Search for You</div>
-        </WingBlank>
+        {
+          this.state.isSearch ? (<WingBlank>
+            <div className={styles.logo}>Search for You</div>
+          </WingBlank>) : ''
+        }
         <SearchBar
           value={this.state.value}
           placeholder="搜索"
-          onSubmit={this.search('submit')}
-          // onClear={value => console.log(value, 'onClear')}
-          // onFocus={() => console.log('onFocus')}
-          onBlur={this.search('blur')}
-          // onCancel={() => console.log('onCancel')}
+          onSubmit={() => this.search('submit')}
+          onClear={value => this.setState({value: ''})}
+          onFocus={() => this.setState({isSearch: false})}
+          onBlur={() => this.setState({isSearch: true})}
           showCancelButton={false}
-          // onChange={this.onChange}
+          onChange={this.onChange}
         />
 
         <WingBlank/>
+
+        <p>{this.state.time}</p>
 
         {
           this.state.isSearch ? (
